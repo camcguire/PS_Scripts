12a7426f7a8d5032dc3fb1a335ad5f7924f36754
camcguire <32575882+camcguire@users.noreply.github.com>
2017-11-07 23:23:33 -0500

    Test Us
    Test these out.

---
+++ delprintpsv2.ps1
@@ -0,0 +1,40 @@
+﻿# Get installed printers
+$printers = Get-WMIObject -Class Win32_Printer | Select Name,PortName
+
+# Load printers to keep
+$tokeep = Import-CSV D:\Scripts\testprintershit.csv -Header ('Name','PortName')
+
+# Set printers to be deleted
+$todelete = Compare-Object $printers $tokeep -Property Name
+
+# Delete the things
+Function Delete-Printers
+{
+	$NetworkPrinters = Get-WmiObject -Class Win32_Printer | Where-Object{$_.Name}
+	If ($NetworkPrinters -ne $null)
+	{
+		Try
+		{
+			Foreach($NetworkPrinter in $todelete)
+			{
+				$NetworkPrinter.Delete()
+				Write-Host "Successfully deleted the network printer:" + $NetworkPrinter.Name -ForegroundColor Green	
+			}
+		}
+		Catch
+		{
+			Write-Host $_
+		}
+	}
+	Else
+	{
+		Write-Warning "Cannot find network printer in the currently environment."
+	}
+}
+
+Delete-Printers
+
+# Clean up after yourself
+Remove-Variable printers -Force
+Remove-Variable tokeep -Force
+Remove-Variable todelete -force
---
+++ delprintpsv3.ps1
@@ -0,0 +1,16 @@
+﻿# Get installed printers
+$printers = Get-Printer | Select Name,PortName
+
+# Load printers to keep
+$tokeep = Import-CSV D:\Scripts\testprintershit.csv -Header ('Name','PortName')
+
+# Set printers to be deleted
+$todelete = Compare-Object $printers $tokeep -Property Name
+
+# Delete the things
+Remove-Printer -Name $todelete.Name
+
+# Clean up after yourself
+Remove-Variable printers -Force
+Remove-Variable tokeep -Force
+Remove-Variable todelete -force
